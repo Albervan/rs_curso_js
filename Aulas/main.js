5c640a10f0b0fb5cd93463d5b068caf6c98d6116
albervan.davies <albervan.davies@siagri.com.br>
2020-09-28 18:32:59 -0400

    Aula 23 (Ãltima) - Utilizando Axios

--- Aulas/main.js
+++ Aulas/main.js
@@ -1,28 +1,10 @@
-var minhaPromise = function() {
-    return new Promise(function(resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://api.github.com/users/albervan');
-        xhr.send(null);
-
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4) {
-                //Verificar o código de sucesso conforme o que identificamos no log Network
-                if (xhr.status === 200) {
-                    resolve(JSON.parse(xhr.responseText));
-                } else {
-                    reject('Erro na requisição');
-                }
-            }
-        }
-    });
-}
-
-minhaPromise()
-    //Se a Promise obtiver sucesso a função "resolve" implementada vai chamar a função "then"
+axios.get('https://api.github.com/users/albervan')
     .then(function(response) {
         console.log(response);
+        console.log(response.request.readyState);
+        console.log(response.data.avatar_url);
     })
-    //Se a Promise não teve sucesso a função "reject" implementada vai chamar a função "catch"
     .catch(function(error) {
         console.warn(error);
-    });
+    }
+);
