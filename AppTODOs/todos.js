0c3c361dc4daca74d4b86e61cc1107918ac95406
albervan.davies <albervan.davies@siagri.com.br>
2020-09-28 12:55:51 -0400

    Aula 19 - AppTODOs - Excluindo Todos

--- AppTODOs/todos.js
+++ AppTODOs/todos.js
@@ -15,7 +15,19 @@ function renderTodos() {
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
 
+        var linkElement = document.createElement('a');
+        linkElement.setAttribute('href', '#');
+
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')');
+
+        var linkText = document.createTextNode('Excluir');
+
+        linkElement.appendChild(linkText);
+
         todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+
         listElement.appendChild(todoElement);
     }
 }
@@ -32,3 +44,8 @@ function addTodo() {
 }
 
 buttonElement.onclick = addTodo;
+
+function deleteTodo(pos) {
+    todos.splice(pos, 1); //Remove uma quantidade de elementos do array a partir de uma posição;
+    renderTodos();
+}
